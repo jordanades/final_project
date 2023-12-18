37abf134aed8b3b8b264e9837aad6872cb6e6c4b
Jordan Ades <jrades44@gmail.com>
2023-12-18 15:29:53 -0500

    FInalized code and comments to make it clear

--- Final.py
+++ Final.py
@@ -1,40 +1,48 @@
-import requests  # Requirement 10.4: Used an API to obtain JSON or other formatted data
-import sys       # Requirement 3.18: Used sys.argv to accept user input at the command line
-import re        # Requirement 7.4: Used a regular expression to check that a string matched a certain pattern
+import requests  
+import sys       
+import re        
 
 def get_pokemon_info(pokemon_name):
-    # Requirement 7.3: Used a 'raw' string in a string to avoid needing to use backslashes
-    # Define the Pokemon API URL
+    # 7.3: Used a 'raw' string in a string to avoid needing to use backslashes
+    # Defined the Pokemon API URL
     api_url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}/'
     
     try:
-        # Requirement 10.4: Used an API to obtain JSON or other formatted data
+        # 10.4: Used an API to obtain JSON or other formatted data
         # Make a GET request to the Pokemon API
         response = requests.get(api_url)
 
-        # Check if the request was successful (status code 200)
+        # Checked here if the request was successful (status code 200 means it was)
         if response.status_code == 200:
-            # Parse the JSON data from the API response
+            # Took JSON data here from the API response
             pokemon_data = response.json()
 
-            # Extract basic information about the Pokemon
+            # Extracted basic height and weight information about the Pokemon
+            # 10.5: Extracted or manipulated values from JSON formatted data.
             name = pokemon_data['name']
             height = pokemon_data['height']
             weight = pokemon_data['weight']
 
-            # Requirement 7.1: Used a string method to split a string into a list of smaller strings
-            # Print the extracted information
-            print(f"Pokemon: {name}")
+            # 7.1: Used a string method to split a string into a list of smaller strings
+            # Converted the Pokemon name into a list of characters
+            name_characters = list(name)
+
+            # Print each character separately
+            print("Pokemon Name Characters:")
+            for char in name_characters:
+                print(char)
+
             print(f"Height: {height} decimetres")
             print(f"Weight: {weight} hectograms")
         else:
             print(f"Error: Unable to retrieve information for {pokemon_name}")
     except Exception as e:
-        # Requirement 7.2: Used a backslash in a string to 'escape' a special character
+        # 7.2: Used a backslash in a string to 'escape' a special character
         print(f"Error: {e} \n")
 
 
 # Check if the correct number of command line arguments is provided
+# 3.18: Used sys.argv to accept user input at the command line
 if len(sys.argv) != 2:
     print("Usage: python Final.py <pokemon_name>")
     sys.exit(1)  # Exit with an error code
@@ -42,10 +50,9 @@ if len(sys.argv) != 2:
 # Get the Pokemon name from the command line arguments
 pokemon_name = sys.argv[1]
 
-# Requirement 7.4: Used a regular expression to check that a string matched a certain pattern
+# 7.4: Used a regular expression to check that a string matched a certain pattern
 # Validate the Pokemon name using a regular expression
 if not re.match("^[a-zA-Z0-9-]*$", pokemon_name):
-    # Requirement 7.5: Used regular expressions with groupings to extract or change parts of a string
     print("Error: Invalid Pokemon name. Only alphanumeric characters and hyphens are allowed.")
     sys.exit(1)  # Exit with an error code
 
